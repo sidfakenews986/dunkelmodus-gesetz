6581af0748ad985390e15a971a319d2d0778c7e0
sidfakenews986 <bisonsicolo@hotmail.com>
2025-10-04 17:58:22 +0100

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+export function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('dark-mode');
+    updateLocalStorage(body.classList.contains('dark-mode'));
+}
+
+function updateLocalStorage(isDarkMode) {
+    localStorage.setItem('darkMode', JSON.stringify(isDarkMode));
+}
+
+function applyInitialMode() {
+    const darkMode = JSON.parse(localStorage.getItem('darkMode')) || false;
+    if (darkMode) {
+        document.body.classList.add('dark-mode');
+    }
+}
+
+window.onload = applyInitialMode;
